02dbeca2b9e25c4e793c4c097500c38b5c347753
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-05-20 20:39:55 +0200

    Update date-picker-custom-control.php
    Removed queueing of the jQuery date picker ui script and added the link() method to the output

--- date/date-picker-custom-control.php
+++ date/date-picker-custom-control.php
@@ -8,14 +8,6 @@ if ( ! class_exists( 'WP_Customize_Control' ) )
 class Date_Picker_Custom_Control extends WP_Customize_Control
 {
     /**
-    * Enqueue the styles and scripts
-    */
-    public function enqueue()
-    {
-        wp_enqueue_style( 'jquery-ui-datepicker' );
-    }
-
-    /**
     * Render the content on the theme customizer page
     */
     public function render_content()
@@ -24,7 +16,7 @@ class Date_Picker_Custom_Control extends WP_Customize_Control
         ?>
             <label>
               <span class="customize-date-picker-control"><?php echo esc_html( $this->label ); ?></span>
-              <input type="date" id="<?php echo $this->id; ?>" name="<?php echo $this->id; ?>" value="<?php echo $this->value(); ?>" class="datepicker" />
+              <input type="date" id="<?php echo $this->id; ?>" name="<?php echo $this->id; ?>" value="<?php echo $this->value(); ?>" class="datepicker" <?php $this->link(); ?> />
             </label>
         <?php
     }
